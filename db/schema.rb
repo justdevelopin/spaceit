289d52832968606fc236f97b3f5b4e5ee753a72f
catriona beamish <mani.motamedifr@gmail.com>
2023-11-20 16:37:35 +0100

    added columns to devise

--- db/schema.rb
+++ db/schema.rb
@@ -60,6 +60,9 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_20_143934) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.string "phone_number"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
