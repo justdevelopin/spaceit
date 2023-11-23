884f574a0c877b7a21496ece052aa1327bec2d6b
catriona beamish <mani.motamedifr@gmail.com>
2023-11-23 16:54:43 +0100

    Finished the reviews and the design

--- app/controllers/spaces_controller.rb
+++ app/controllers/spaces_controller.rb
@@ -7,19 +7,21 @@ class SpacesController < ApplicationController
   def show
     @space = Space.find(params[:id])
     @booking = Booking.new
+    @reviews = @space.reviews
+    @review = Review.new
   end
 
   def create
-    @space = space.new
+    @space = Space.new
   end
 
   def new
     @space = Space.new
+  end
 
   private
 
   def space_params
     params.require(:space).permit(:name, :description, :image, :other_attributes)
   end
-end
 end
