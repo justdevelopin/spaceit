3bc8cc0ad967e74d48be963a150107175081d079
jordi perez <jlpmccs@gmail.com>
2023-11-24 12:42:57 +0100

    Saving changes before merge

--- config/routes.rb
+++ config/routes.rb
@@ -6,13 +6,9 @@ Rails.application.routes.draw do
     resources :bookings, only: [:create]
     resources :reviews, only: [:create]
   end
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  # Add this line for a non-nested route to handle the index action
+  resources :bookings, only: [:index]
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  # Other routes...
 end
