6c47d9500c71079f1235910d754df1a43c8541f9
jordi perez <jlpmccs@gmail.com>
2023-11-24 15:18:44 +0100

    profilehalffinished

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,6 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-
     @spaces = @user.spaces
     @bookings = @user.bookings
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
+
+  has_one_attached :picture
 end
